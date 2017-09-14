d6899a2fdfcdaab29b3af8b34f4cf662b6c7b6a9
weifucheng <244157602@qq.com>
2017-09-14 23:31:32 +0800

    论坛主页添加

---
+++ main/src/main/java/com/example/main/controller/TestRedis.java
@@ -0,0 +1,21 @@
+package com.example.main.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RestController
+public class TestRedis {
+
+    @RequestMapping("/set")
+    public String set(HttpServletRequest request,String name){
+        request.getSession().setAttribute("name",name);
+        return name+"---"+request.getSession().getAttribute("name");
+    }
+
+    @RequestMapping("/get")
+    public String get(HttpServletRequest request){
+       return request.getSession().getAttribute("name").toString();
+    }
+}
---
+++ user/src/main/java/com/example/user/config/RedisSessionConfig.java
@@ -0,0 +1,10 @@
+package com.example.user.config;
+
+
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+
+@EnableRedisHttpSession
+public class RedisSessionConfig {
+
+}
