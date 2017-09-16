a40fe95b86d7369a0c2adfbf34de2be228b39052
weifucheng <244157602@qq.com>
2017-09-16 22:51:17 +0800

    添加主页和校园动态

---
+++ xydt/src/main/java/com/example/xydt/XydtApplication.java
@@ -0,0 +1,12 @@
+package com.example.xydt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XydtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XydtApplication.class, args);
+	}
+}
