4c4158a55c349298688294af1823d8b5a72a7d05
weifucheng <244157602@qq.com>
2017-09-16 20:29:37 +0800

    main的主页更新

--- main/src/main/java/com/example/main/controller/MainController.java
+++ main/src/main/java/com/example/main/controller/MainController.java
@@ -2,6 +2,7 @@ package com.example.main.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -10,7 +11,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class MainController {
 
     @RequestMapping("/index")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("username","张三");
         return "index";
     }
 }
